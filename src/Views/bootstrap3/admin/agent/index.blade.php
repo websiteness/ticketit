23db9c2f0f56ed2558b913c3d05509a6dc520df9
mvsiaga <websiteness1@gmail.com>
2020-09-14 13:58:29 +0800

    Add Agent - moved to index

--- src/Views/bootstrap3/admin/agent/index.blade.php
+++ src/Views/bootstrap3/admin/agent/index.blade.php
@@ -4,19 +4,43 @@
     {{ trans('ticketit::admin.agent-index-title') }}
 @stop
 
+@section('header_styles')
+	<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+@stop
+
 @section('content')
     @include('ticketit::shared.header')
     <div class="panel panel-default">
         <div class="panel-heading">
             <h2>{{ trans('ticketit::admin.agent-index-title') }}
-                {!! link_to_route(
+                {{-- {!! link_to_route(
                                     $setting->grab('admin_route').'.agent.create',
                                     trans('ticketit::admin.btn-create-new-agent'), null,
                                     ['class' => 'btn btn-primary pull-right'])
-                !!}
+                !!} --}}
             </h2>
         </div>
-
+        <div class="panel panel-default filters-panel">
+            <div class="panel-body">
+            {!! CollectiveForm::open(['route'=> $setting->grab('admin_route').'.agent.store', 'method' => 'POST']) !!}
+                <ul class="nav nav-pills">
+                    <li role="presentation">
+                        <div class="form-group">
+                            <select class="form-control select2" id="agent" name="agent" required>
+                                <option value="">Select User</option>
+                                @foreach($users as $user)
+                                <option value="{{ $user->id }}">{{ $user->first_name . ' ' . $user->last_name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </li>
+                    <li role="presentation">
+                        <button type="submit" class="btn btn-success">Add</button>
+                    </li>
+                </ul>
+            {!! CollectiveForm::close() !!}
+            </div>
+        </div>
         @if ($agents->isEmpty())
             <h3 class="text-center">{{ trans('ticketit::admin.agent-index-no-agents') }}
                 {!! link_to_route($setting->grab('admin_route').'.agent.create', trans('ticketit::admin.agent-index-create-new')) !!}
@@ -86,4 +110,13 @@
 
         @endif
     </div>
+@stop
+
+@section('footer')
+	<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+	<script>
+		$(document).ready(function() {
+			$('.select2').select2();
+		});
+    </script>
 @stop
