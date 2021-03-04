10b020a4b238ee0df45ac288814a7d004e8c075a
websiteness <websiteness1@gmail.com>
2021-03-04 12:31:01 +0800

    updated summernote version to fix bug

--- src/Helpers/Cdn.php
+++ src/Helpers/Cdn.php
@@ -11,7 +11,7 @@ namespace Kordy\Ticketit\Helpers;
 class Cdn
 {
     const CodeMirror = '5.40.0';
-    const Summernote = '0.8.10';
+    const Summernote = '0.8.18';
     const FontAwesome = '4.7.0';
     const FontAwesome5 = '5.3.1';
 
