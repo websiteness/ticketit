66d007a2cf08032f613ba43975c2b2cbdf8b3948
Elaine Moskera <maimoskera@gmail.com>
2022-07-25 15:40:18 +0800

    check if ticket has value for clickup item id

--- src/Controllers/CommentsController.php
+++ src/Controllers/CommentsController.php
@@ -111,8 +111,10 @@ class CommentsController extends Controller
         }
 
         //Create comment on clickup
-        $clickup_service = new ClickupService;
-        $clickup_service->saveComment($comment, $ticket, $request->status_change);
+        if(!empty($ticket->clickup_item_id)) {
+            $clickup_service = new ClickupService;
+            $clickup_service->saveComment($comment, $ticket, $request->status_change);
+        }
 
         $update_ticket = true;
 
@@ -183,8 +185,10 @@ class CommentsController extends Controller
         $comment->save();
 
         //Update comment on clickup
-        $clickup_service = new ClickupService;
-        $clickup_service->saveComment($comment);
+        if(!empty($ticket->clickup_item_id)) {
+            $clickup_service = new ClickupService;
+            $clickup_service->saveComment($comment);
+        }
 
         return redirect()->back();
     }
@@ -201,8 +205,10 @@ class CommentsController extends Controller
         $comment = Comment::find($id);
 
         //Delete comment on clickup
-        $clickup_service = new ClickupService;
-        $clickup_service->saveComment($comment);
+        if(!empty($ticket->clickup_item_id)) {
+            $clickup_service = new ClickupService;
+            $clickup_service->saveComment($comment);
+        }
 
         $comment->delete();
 
